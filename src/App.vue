913ea1a445e1a7ceb8c6aeddf80e4709719b981a
Manasa Mudunuru <Manasa.Mudunuru@nttdata.com>
2024-04-30 16:27:54 +0530

    Initialfilesadd

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg=""/>
+   <h1>Hello, Welcome to Your Vue.js App</h1>
+      <p>Congratulations! Your app is running. 🎉</p>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+ 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
